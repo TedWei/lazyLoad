69947581125b12046239f1091c3c05c754dc21cd
weited <youcancallmetedwei@gmail.com>
2015-08-11 18:07:02 +0800

    change file load
    将原来的并行下载更改了

--- lazyLoad.js
+++ lazyLoad.js
@@ -7,41 +7,48 @@
     //  loadScript(['jQuery.js','bootstrap.js'],function(){
     //	   /*do something in here*/
     //	})
-    var loadScript = function(script,callback){
-    	if (!script){
-    		console.error("not found script");
-    		return;
-    	}
-    	var loadS = function(url,callB){
+    var loadScript = function(script_url, callback) {
+        if (!script_url) {
+            console.error("not found script");
+            return;
+        }
+        var tmpArray = [];
+        var loadS = function(url, callB) {
             var newScript = document.createElement('script');
             newScript.type = "text/javascript";
-            if (newScript.readyState){
-            	newScript.onreadystatechange = function(){
-            		if (newScript.readyState === 'loaded' || newScript.readyState === 'complete'){
-            			newScript.onreadystatechange = null;
-            			callB();
-            		}
-            	};
-            }else{
-            	newScript.onload = function(){
-                    callB()
-            	};
+            if (newScript.readyState) {
+                newScript.onreadystatechange = function() {
+                    if (newScript.readyState === 'loaded' || newScript.readyState === 'complete') {
+                        newScript.onreadystatechange = null;
+                        tmpArray.shift(url);
+                        if (!tmpArray.length) {
+                            callB();
+                        }
+                    }
+                };
+            } else {
+                newScript.onload = function() {
+                    tmpArray.shift(url);
+                    if (!tmpArray.length) {
+                        callB();
+                    }
+                };
             }
             newScript.src = url;
             document.getElementsByTagName('head')[0].appendChild(newScript);
-    	};
-        if (Object.prototype.toString.apply(script) === '[object Array]'){
-            (function loadSIter(){
-            	loadS(script[0],function(){
-            	    script.shift();
-            	    if (script.length === 0){
-            	        callback();
-            	    }else{
-            	        loadSIter();
-            	    }
-            	})
-            })()
-        }else{
-            loadS(script,callback);
+        };
+
+        var init = function() {
+            if (Object.prototype.toString.apply(script_url) === '[object Array]') {
+                tmpArray = script_url;
+            } else if (typeof script_url === "string") {
+                tmpArray.push(script_url);
+            }
+
+            for (var i = tmpArray.length - 1; i >= 0; i--) {
+                loadS(tmpArray[i], callback);
+            }
         }
+
+        init();
     }
